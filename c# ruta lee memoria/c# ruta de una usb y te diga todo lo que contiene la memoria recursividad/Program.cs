c1fea38e34f752c7f8296252be0477ab9935299b
AlejandroVelezGuillermo <125221168+AlejandroVelezGuillermo@users.noreply.github.com>
2023-08-31 17:04:40 -0600

    Add files via upload

---
+++ c# ruta lee memoria/c# ruta de una usb y te diga todo lo que contiene la memoria recursividad/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System;
+
+    Console.Write("Ingresa la ruta de la memoria USB: ");
+    string usbPath = Console.ReadLine();
+
+    if (Directory.Exists(usbPath))
+    {
+        Console.WriteLine("Contenido de la memoria USB:");
+
+        string[] directories = Directory.GetDirectories(usbPath);
+        foreach (string directory in directories)
+        {
+            Console.WriteLine($"[Directorio] {Path.GetFileName(directory)}");
+        }
+
+        string[] files = Directory.GetFiles(usbPath);
+        foreach (string file in files)
+        {
+            Console.WriteLine($"[Archivo] {Path.GetFileName(file)}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("La ruta de la memoria USB no es válida.");
+    }
